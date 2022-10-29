0182e7738e87481ad09104f04b9b87f2c5af710f
Woong <jiyko7@kyonggi.ac.kr>
2022-10-29 14:28:24 +0900

    횡스크롤 러닝 게임
    횡스크롤 러닝 게임

---
+++ Assets/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float Level (int score)
+    {
+        int defaultSpeed = -5;
+        int increspeed = (score * -1) / 100;
+        if (increspeed < -5)
+            increspeed = -5;
+        return defaultSpeed + increspeed;
+    }
+}
